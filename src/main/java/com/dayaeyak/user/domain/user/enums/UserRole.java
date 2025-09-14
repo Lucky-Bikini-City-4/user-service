37e7c11494a77a2f919c2a27b0c09cb887ee1bd3
legoChoi <chlx159@gmail.com>
2025-09-14 09:11:29 +0900

    refactor: 요청 바디 UserRole 값 바운딩을 위한 @JsonCreator 추가

--- src/main/java/com/dayaeyak/user/domain/user/enums/UserRole.java
+++ src/main/java/com/dayaeyak/user/domain/user/enums/UserRole.java
@@ -1,5 +1,8 @@
 package com.dayaeyak.user.domain.user.enums;
 
+import com.dayaeyak.user.common.exception.CustomRuntimeException;
+import com.dayaeyak.user.common.exception.type.UserExceptionType;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -16,10 +19,11 @@ public enum UserRole {
 
     private final String role;
 
+    @JsonCreator
     public static UserRole of(String role) {
         return Stream.of(UserRole.values())
                 .filter(userRole -> userRole.role.equalsIgnoreCase(role))
                 .findFirst()
-                .orElseThrow(RuntimeException::new);
+                .orElseThrow(() -> new CustomRuntimeException(UserExceptionType.INVALID_USER_ROLE));
     }
 }
