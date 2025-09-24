04a32821b8087a1503161e3f5fa16fce366fef8b
legoChoi <chlx159@gmail.com>
2025-09-24 21:08:27 +0900

    feat: 소셜 회원가입 API 구현

--- src/main/java/com/dayaeyak/user/domain/user/querydsl/UserQuerydslRepository.java
+++ src/main/java/com/dayaeyak/user/domain/user/querydsl/UserQuerydslRepository.java
@@ -1,10 +1,13 @@
 package com.dayaeyak.user.domain.user.querydsl;
 
+import com.dayaeyak.user.domain.user.User;
 import com.dayaeyak.user.domain.user.enums.SearchType;
 import com.dayaeyak.user.domain.user.enums.UserRole;
 import com.dayaeyak.user.domain.user.querydsl.dto.response.UserSearchProjectionDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
 
 public interface UserQuerydslRepository {
 
@@ -15,4 +18,10 @@ public interface UserQuerydslRepository {
             String keyword,
             SearchType searchType
     );
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+
+    boolean existsByPhone(String phone);
 }
