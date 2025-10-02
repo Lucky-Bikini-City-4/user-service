0a0bf1e27d8bccdf47f77c149c6a687e23d8eceb
legoChoi <chlx159@gmail.com>
2025-10-02 12:08:31 +0900

    refactor: 쿼리 수정

--- src/main/java/com/dayaeyak/user/domain/user/jpa/UserJpaRepository.java
+++ src/main/java/com/dayaeyak/user/domain/user/jpa/UserJpaRepository.java
@@ -3,14 +3,16 @@ package com.dayaeyak.user.domain.user.jpa;
 import com.dayaeyak.user.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface UserJpaRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-
-    boolean existsByNickname(String nickname);
-
-    boolean existsByPhone(String phone);
 
     Optional<User> findByEmail(String email);
+
+    boolean existsByEmailAndDeletedAtIsNull(String email);
+
+    boolean existsByNicknameAndDeletedAtIsNull(String nickname);
+
+    boolean existsByPhoneAndDeletedAtIsNull(String phone);
 }
